76597ce69ca6d0cb4a6f548b16401bcfebf352ac
Dawn Dream/xm2020 <64729945+Dawn-Dream@users.noreply.github.com>
2020-11-04 18:45:19 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
-import socket               # 导入 socket 模块
+import socket               
  
-s = socket.socket()         # 创建 socket 对象
-host = socket.gethostname() # 获取本地主机名
-port = 12345                # 设置端口号
+s = socket.socket()        
+host = socket.gethostname() 
+port = 12345           
  
 s.connect((host, port))
 print s.recv(1024)
