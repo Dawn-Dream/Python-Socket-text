df4b87060e73c8e467ab2becc529b83f4ab7a054
Dawn Dream/xm2020 <64729945+Dawn-Dream@users.noreply.github.com>
2020-11-04 18:44:42 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
-import socket               # 导入 socket 模块
+import socket               
  
-s = socket.socket()         # 创建 socket 对象
-host = socket.gethostname() # 获取本地主机名
-port = 12345                # 设置端口
-s.bind((host, port))        # 绑定端口
+s = socket.socket()         
+host = socket.gethostname() 
+port = 12345             
+s.bind((host, port))        
  
-s.listen(5)                 # 等待客户端连接
+s.listen(5)                
 while True:
-    c,addr = s.accept()     # 建立客户端连接
-    print '连接地址：', addr
-    c.send('欢迎访问菜鸟教程！')
-    c.close()                # 关闭连接
+    c,addr = s.accept()     
+    print 'ip:', addr
+    c.send('wdnmd!')
+    c.close()               
